41dfe421118e43092d718685bb4ce58b60c84146
Talha <sufiyan.talha1295@gmail.com>
2018-10-12 12:22:37 -0700

    initial project for SASS support using webpack

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './sass/main.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <p className='test'>Dyanmic</p>
+      </div>
+    );
+  }
+}
+
+
+export default App;
